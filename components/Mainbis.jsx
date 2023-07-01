21b132e727805f52ee377e2702470d0ad69347c4
Elodieguay <e.guayroso@gmail.com>
2023-07-02 00:09:04 +0200

    visible Main and responsive mobile

--- components/Mainbis.jsx
+++ components/Mainbis.jsx
@@ -7,19 +7,21 @@ const Mainbis = () => {
   return (
     <div id='home'className='bg5 w-full min-h-screen text-center flex items-center '>
         <div className=' max-w-5xl m-auto md:grid grid-cols-2 gap-7  '>
-            <div className='me max-w-5xl m-auto p-5 justify-center  '>
+            <div className='me max-w-5xl m-auto  justify-center  '>
                 {/* <p className=' uppercase text-white text-xl font-semibold py-3'>Je suis</p> */}
-                <h1 className=' uppercase font-semibold text-6xl tracking-normal text-[#0f3b56] py-3'>elodie guayroso</h1>
-                <p className=' uppercase text-[#0f3b56] text-xl font-semibold py-3 m-2'>étudiante developpeuse web fullstack </p>
+                {/* <h1 className=' uppercase font-semibold text-4xl tracking-normal text-[#0f3b56]  mt-40 pt-96'>elodie guayroso</h1> */}
             </div>
-            <ul  className='max-w-5xl m-auto p-2 flex-col justify-center space-x-5 '>
-                <li className='surlignev uppercase  font-bold text-7xl tracking-normal text-[#0f3b56] m-5 mr-2'>
+            <ul  className='max-w-5xl m-auto pr-4 flex-col justify-center space-x-5 p-2'>
+                <li className='surlignev uppercase  font-bold text-7xl tracking-normal text-[#0f3b56] m-6'>
                     <a className=' cursor-none' href='#about'  >hello!</a></li>
-                <li className='surligney uppercase font-bold text-7xl tracking-normal text-[#0f3b56] m-5 m-2'>
+                <li className='surligney uppercase font-bold text-7xl tracking-normal text-[#0f3b56] m-6'>
                     <a className=' cursor-none' href='#projects'>Projets.</a> </li>
-                <li className='surligner uppercase font-bold text-7xl tracking-normal text-[#0f3b56] m-5'>
+                <li className='surligner uppercase font-bold text-7xl tracking-normal text-[#0f3b56] m-6 '>
                     <a className=' cursor-none' href='#contact'> Contact.</a></li>
                 <li><Socials/></li>
+                <p className='  text-[#0f3b56] text-xl font-semibold pt-10 '>Etudiante developpeuse web fullstack </p>
+                <h1 className='  font-semibold text-4xl tracking-normal text-[#0f3b56]  '>Elodie Guayroso</h1>
+
             </ul>
         </div>
     </div>
