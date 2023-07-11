edfb4d7b26e7d9976385eb207003c4376ad0e5b6
Elodieguay <e.guayroso@gmail.com>
2023-07-11 18:33:47 +0200

    final presentation video

--- components/Video.jsx
+++ components/Video.jsx
@@ -4,16 +4,7 @@
 import ReactPlayer from 'react-player';
 
 export default function Player(props) {
-//   const stylePlayer ={
-//     '@media(min-width:840px)' : {
-//         width: '1000px',
-//         height: '750px'
-//     } ,
-//     '@media(max-width:640px)' : {
-//         width:'100%',
-//         height:'100%'
-//     } 
-//   }
+
   const videoURL = "https://www.youtube.com/watch?v=" + props.videoId
   return(
       <div className='video-wrapper' >
