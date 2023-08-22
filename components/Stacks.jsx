e372d1c659e8fbeec1b41a0c57d1c50a55522742
Elodieguay <e.guayroso@gmail.com>
2023-08-22 19:33:02 +0200

    stacks maj et cv maj

--- components/Stacks.jsx
+++ components/Stacks.jsx
@@ -7,22 +7,32 @@ import {SiFigma} from "react-icons/si"
 import {SiGithub} from "react-icons/si"
 import {SiNodedotjs} from "react-icons/si"
 import {SiMysql} from "react-icons/si"
-
+import {SiPhp} from "react-icons/si"
+import {SiLaravel} from 'react-icons/si'
+import {SiPostgresql} from 'react-icons/si'
+import {SiDocker} from 'react-icons/si'
 
 
 
 const Stacks = () => {
   return (
+    <>
     <div className='social flex justify-between items-center max-w-xs m-auto py-2'>
-    <SiJavascript size={25} color='#0B927A'/>
-    <SiReact size={25} color='#0B927A'/>
-    <SiNextdotjs size={25} color='#0B927A'/>
-    <SiTailwindcss size={25} color='#0B927A'/>
-    <SiFigma size={25} color='#0B927A'/>
-    <SiGithub size={25} color='#0B927A'/>
-    <SiNodedotjs size={25} color='#0B927A'/>
-    <SiMysql size={25} color='#0B927A'/>
+      <SiJavascript size={25} color='#0B927A'/>
+      <SiReact size={25} color='#0B927A'/>
+      <SiNextdotjs size={25} color='#0B927A'/>
+      <SiPhp size={25} color='#0B927A'/>
+      <SiLaravel size={25} color='#0B927A'/>
+      <SiTailwindcss size={25} color='#0B927A'/>
+      <SiFigma size={25} color='#0B927A'/>
+      <SiGithub size={25} color='#0B927A'/>
+      <SiNodedotjs size={25} color='#0B927A'/>
+      <SiMysql size={25} color='#0B927A'/>
+      <SiPostgresql size={25} color='#0B927A'/>
+      <SiDocker size={25} color='#0B927A'/>
+
     </div>
+    </>
   )
 }
 
